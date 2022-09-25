121c90b43dd8656e1c616095b6501f7c6ce9894b
Eric Callanan <calleric@users.noreply.github.com>
2022-09-25 21:10:36 +0100

    added Circular Slider

---
+++ CircularSliderView.swift
@@ -0,0 +1,133 @@
+///
+/// CircularSliderView.swift
+/// How to create a Circular Slider in SwiftUI
+///
+/// Xcode version 14.0
+/// Swift version 5
+///
+/// Created by Eric Callanan on 25 / September / 2022.
+///
+///
+/// see more
+/// https://swdevnotes.com/swift/2022/create-a-circular-slider-in-swiftui/
+///
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var progress1 = 0.75
+    @State var progress2 = 37.5
+    @State var progress3 = 7.5
+    
+    var body: some View {
+        ZStack {
+            Color(hue: 0.58, saturation: 0.06, brightness: 1.0)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                CircularSliderView_XX(value: $progress1)
+                    .frame(width:250, height: 250)
+                
+                HStack {
+                    CircularSliderView_XX(value: $progress2, in: 32...50)
+
+                    CircularSliderView_XX(value: $progress3, in: 0...100)
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CircularSliderView: View {
+    @Binding var progress: Double
+
+    @State private var rotationAngle = Angle(degrees: 0)
+    private var minValue = 0.0
+    private var maxValue = 1.0
+    
+    init(value progress: Binding<Double>, in bounds: ClosedRange<Int> = 0...1) {
+        self._progress = progress
+        
+        self.minValue = Double(bounds.first ?? 0)
+        self.maxValue = Double(bounds.last ?? 1)
+        self.rotationAngle = Angle(degrees: progressFraction * 360.0)
+    }
+    
+    private var progressFraction: Double {
+        return ((progress - minValue) / (maxValue - minValue))
+    }
+    
+    private func changeAngle(location: CGPoint) {
+        // Create a Vector for the location (reversing the y-coordinate system on iOS)
+        let vector = CGVector(dx: location.x, dy: -location.y)
+        
+        // Calculate the angle of the vector
+        let angleRadians = atan2(vector.dx, vector.dy)
+        
+        // Convert the angle to a range from 0 to 360 (rather than having negative angles)
+        let positiveAngle = angleRadians < 0.0 ? angleRadians + (2.0 * .pi) : angleRadians
+        
+        // Update slider progress value based on angle
+        progress = ((positiveAngle / (2.0 * .pi)) * (maxValue - minValue)) + minValue
+        rotationAngle = Angle(radians: positiveAngle)
+    }
+    
+    var body: some View {
+        GeometryReader { gr in
+            let radius = (min(gr.size.width, gr.size.height) / 2.0) * 0.9
+            let sliderWidth = radius * 0.1
+            
+            VStack(spacing:0) {
+                ZStack {
+                    Circle()
+                        .stroke(Color(hue: 0.0, saturation: 0.0, brightness: 0.9),
+                                style: StrokeStyle(lineWidth: sliderWidth))
+                        .overlay() {
+                            Text("\(progress, specifier: "%.2f")")
+                                .font(.system(size: radius * 0.6, weight: .bold, design:.rounded))
+                        }
+                    // uncomment to show tick marks
+                    //Circle()
+                    //    .stroke(Color(hue: 0.0, saturation: 0.0, brightness: 0.6),
+                    //            style: StrokeStyle(lineWidth: sliderWidth * 0.75,
+                    //                               dash: [2, (2 * .pi * radius)/24 - 2]))
+                    //    .rotationEffect(Angle(degrees: -90))
+                    Circle()
+                        .trim(from: 0, to: progressFraction)
+                        .stroke(Color(hue: 0.0, saturation: 0.5, brightness: 0.9),
+                                style: StrokeStyle(lineWidth: sliderWidth, lineCap: .round)
+                        )
+                        .rotationEffect(Angle(degrees: -90))
+                    Circle()
+                        .fill(Color.white)
+                        .shadow(radius: (sliderWidth * 0.3))
+                        .frame(width: sliderWidth, height: sliderWidth)
+                        .offset(y: -radius)
+                        .rotationEffect(rotationAngle)
+                        .gesture(
+                            DragGesture(minimumDistance: 0.0)
+                                .onChanged() { value in
+                                    changeAngle(location: value.location)
+                                }
+                        )
+                }
+                .frame(width: radius * 2.0, height: radius * 2.0, alignment: .center)
+                .padding(radius * 0.1)
+            }
+            
+            .onAppear {
+                self.rotationAngle = Angle(degrees: progressFraction * 360.0)
+            }
+        }
+    }
+}
+
