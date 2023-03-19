1a319eafa26508a4c721fce36b3694a4d68a4441
Eric Callanan <calleric@users.noreply.github.com>
2023-03-19 09:43:41 +0000

    added init to TaskViewModel

--- TasksAppWithChatGpt/TasksAppWithChatGpt/TaskViewModel.swift
+++ TasksAppWithChatGpt/TasksAppWithChatGpt/TaskViewModel.swift
@@ -8,11 +8,15 @@
 import Foundation
 
 class TaskViewModel: ObservableObject {
-    @Published var tasks = [
-        Task(title: "Task 1", dueDate: Date(), status: .inProgress),
-        Task(title: "Task 2", dueDate: Date().addingTimeInterval(86400), status: .notStarted),
-        Task(title: "Task 3", dueDate: nil, status: .complete)
-    ]
+    @Published var tasks: [Task]
     @Published var isEditing = false // add isEditing property
+    
+    init() {
+        tasks = [
+            Task(title: "Task 1", dueDate: Date(), status: .inProgress),
+            Task(title: "Task 2", dueDate: Date().addingTimeInterval(86400), status: .notStarted),
+            Task(title: "Task 3", dueDate: nil, status: .complete)
+        ]
+    }
 }
 
