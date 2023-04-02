a6da7d1ba14717a160cf10c5e9e0f79b3e2537e2
Eric Callanan <calleric@users.noreply.github.com>
2023-04-02 16:15:18 +0100

    fixed coddle issue - but now initialisation error

--- TasksAppWithChatGpt/TasksAppWithChatGpt/Task.swift
+++ TasksAppWithChatGpt/TasksAppWithChatGpt/Task.swift
@@ -13,15 +13,40 @@ enum TaskStatus: String, CaseIterable {
     case complete = "Complete"
 }
 
-struct Task: Identifiable, Equatable {
+struct Task: Identifiable, Equatable, Codable {
     var id = UUID()
     var title: String
-    var completed: Bool = false
     var dueDate: Date?
-    var status: TaskStatus = .notStarted
+    var status: TaskStatus
+    var completed = false
     
-    static func == (lhs: Task, rhs: Task) -> Bool {
-        return lhs.id == rhs.id
+    enum CodingKeys: String, CodingKey {
+        case id, title, dueDate, status, completed
+    }
+    
+    init(id: UUID = UUID(), title: String, dueDate: Date?, status: TaskStatus, completed: Bool = false) {
+        self.id = id
+        self.title = title
+        self.dueDate = dueDate
+        self.status = status
+        self.completed = completed
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        dueDate = try container.decodeIfPresent(Date.self, forKey: .dueDate)
+        status = try container.decode(TaskStatus.self, forKey: .status)
+        completed = try container.decode(Bool.self, forKey: .completed)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(title, forKey: .title)
+        try container.encodeIfPresent(dueDate, forKey: .dueDate)
+        try container.encode(status, forKey: .status)
+        try container.encode(completed, forKey: .completed)
     }
 }
-
